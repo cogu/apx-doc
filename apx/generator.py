8fbcbb453d2fe70df3374fd7d8f446be283eda80
cogu <congus8@gmail.com>
2017-01-26 23:59:45 +0100

    fixed some bugs in the NodeGenerator

--- apx/generator.py
+++ apx/generator.py
@@ -2,19 +2,23 @@ import apx.base
 import cfile as C
 
 class SignalInfo:
-   def __init__(self, offset, pack_len, func, dsg, init_value=None):
+   def __init__(self, offset, pack_len, func, dsg, operation, init_value=None):
       self.offset=offset
       self.pack_len=pack_len
       self.init_value=init_value
       self.func=func
       self.dsg=dsg
+      if operation == 'pack' or operation == 'unpack':
+         self.operation=operation
+      else:
+         raise ValueError("operation: invalid parameter value '%s', expected 'pack' or 'unpack'"%operation)
         
 
 class NodeGenerator:
    def __init__(self):
       self.includes=None
 
-   def genPackInteger(self, code,buf,valname,dsg,localvar,offset,indent):   
+   def genPackUnpackInteger(self, code, buf, operation, valname, dsg, localvar, offset, indent):   
       dataLen=0
       resolvedDsg = dsg.resolveType()
       if resolvedDsg.data['type']=='c' or resolvedDsg.data['type']=='C':
@@ -22,25 +26,34 @@ class NodeGenerator:
          basetype='sint8' if resolvedDsg.data['type']=='c' else 'uint8'
       elif resolvedDsg.data['type']=='s' or resolvedDsg.data['type']=='S':
          dataLen=2
-         basetype='sint16' if resolvedDsg.data['type']=='c' else 'uint16'
+         basetype='sint16' if resolvedDsg.data['type']=='s' else 'uint16'
       elif resolvedDsg.data['type']=='l' or resolvedDsg.data['type']=='L':
          dataLen=4
-         basetype='sint32' if resolvedDsg.data['type']=='c' else 'uint32'
+         basetype='sint32' if resolvedDsg.data['type']=='l' else 'uint32'
       else:
          raise NotImplementedError(resolvedDsg.data['type'])
       if 'bufptr' in localvar:
          #relative addressing
-         code.append(C.statement('packLE(%s,(uint32) %s,(uint8) sizeof(%s))'%(localvar['bufptr'].name,valname,basetype),indent=indent))
+         if operation == 'pack':
+            code.append(C.statement('packLE(%s,(uint32) %s,(uint8) sizeof(%s))'%(localvar['bufptr'].name,valname,basetype),indent=indent))
+         else:
+            code.append(C.statement('%s = (%s) unpackLE(%s,(uint8) sizeof(%s))'%(valname, basetype, localvar['bufptr'].name, basetype), indent=indent))
          code.append(C.statement('%s+=(uint8) sizeof(%s)'%(localvar['bufptr'].name,basetype),indent=indent))
       else:
          #absolute addressing
          if dataLen==1:
-            code.append(C.statement("%s[%d]=(uint8) %s"%(buf.name,offset,valname),indent=indent))
-         else:         
-            code.append(C.statement('packLE(&%s[%d],(uint32) %s,(uint8) %du)'%(buf.name,offset,valname,dataLen),indent=indent))
+            if operation == 'pack':
+               code.append(C.statement("%s[%d]=(uint8) %s"%(buf.name,offset,valname),indent=indent))
+            else: #unpack               
+               code.append(C.statement("*%s = (%s) %s[%d]"%(valname, basetype, buf.name, offset),indent=indent))
+         else:
+            if operation == 'pack':
+               code.append(C.statement('packLE(&%s[%d],(uint32) %s,(uint8) %du)'%(buf.name,offset,valname,dataLen),indent=indent))
+            else: #unpack
+               code.append(C.statement('*%s = (%s) unpackLE(&%s[%d],(uint8) %du)'%(valname, basetype, buf.name, offset, dataLen),indent=indent))
       return dataLen
    
-   def genPackItem(self, code,buf,val,dsg,localvar,offset,indent,indentStep):
+   def genPackUnpackItem(self, code, buf, operation, val, dsg, localvar, offset, indent, indentStep):
       packLen=0
       if isinstance(val,C.variable):
          valname=val.name
@@ -53,17 +66,28 @@ class NodeGenerator:
          if dsg.data['type'].startswith('a'): #string         
             if 'bufptr' in localvar:
                #use relative addressing using 'p' pointer
-               code.append(C.statement('memcpy(%s,%s,%d)'%(localvar['bufptr'].name,valname,dsg.data['arrayLen']),indent=indent))
+               if operation == 'pack':
+                  if dsg.data['arrayLen']>1:
+                     code.append(C.statement('memcpy(%s,%s,%d)'%(localvar['bufptr'].name,valname,dsg.data['arrayLen']-1),indent=indent))
+                  code.append(C.statement("%s[%d]='\0'"%(localvar['bufptr'].name,dsg.data['arrayLen']),indent=indent))
+               else:
+                  code.append(C.statement('memcpy(%s,%s,%d)'%(valname,localvar['bufptr'].name,dsg.data['arrayLen']),indent=indent))
                code.append(C.statement('%s+=%d'%(localvar['bufptr'].name,dsg.data['arrayLen']),indent=indent))
-            else:
+            else:               
                #use absolute addressing using buf variable and offset
-               code.append(C.statement('memcpy(&%s[%d],%s,%d)'%(buf.name,offset,valname,dsg.data['arrayLen']),indent=indent))
+               if operation == 'pack':
+                  if dsg.data['arrayLen']>1:
+                     code.append(C.statement('memcpy(&%s[%d],%s,%d)'%(buf.name,offset,valname,dsg.data['arrayLen']-1),indent=indent))
+                  code.append(C.statement("%s[%d]='\0')"%(buf.name,dsg.data['arrayLen']),indent=indent))
+               else:
+                  code.append(C.statement('memcpy(%s,&%s[%d],%d)'%(valname,buf.name,offset,dsg.data['arrayLen']),indent=indent))
             packLen=dsg.data['arrayLen']
          elif dsg.data['type']=='record':
             if 'bufptr' not in localvar:
                localvar['bufptr']=C.variable('p','uint8',pointer=True)      
             for elem in dsg.data['elements']:                     
                if isinstance(val,C.variable):
+                  #TODO: replace following lines with call to user hook that instead applies the _RE-rule to record elements
                   if val.pointer:
                      childName="%s->%s_RE"%(valname,elem['name'])
                   else:
@@ -71,7 +95,7 @@ class NodeGenerator:
                elif isinstance(val,str):
                   childName="%s.%s"%(valname,elem['name'])
                assert(elem is not None)
-               itemLen=genPackItem(code,buf,childName,apx.ApxDataSignature(elem),localvar,offset,indent,indentStep)
+               itemLen=genPackUnpackItem(code, buf, operation, childName, apx.ApxDataSignature(elem), localvar, offset, indent, indentStep)
                offset+=itemLen
                packLen+=itemLen
          elif dsg.isArray():      
@@ -93,14 +117,14 @@ class NodeGenerator:
             code.append(C.statement('for({0}=0;{0}<{1};{0}++)'.format(localvar['loopVar'].name,dsg.data['arrayLen']),indent=indent))
             block=C.block(indent=indent)
             indent+=indentStep
-            itemLen=genPackItem(block,buf,valname+'[%s]'%localvar['loopVar'].name,childType,localvar,offset)
+            itemLen=genPackUnpackItem(block, buf, operation, valname+'[%s]'%localvar['loopVar'].name, childType, localvar, offset)
             indent-=indentStep
             code.append(block)         
       else:
-         packLen=self.genPackInteger(code,buf,valname,dsg,localvar,offset,indent)      
+         packLen=self.genPackUnpackInteger(code, buf, operation, valname, dsg, localvar, offset, indent)
       return packLen
    
-   def genPackFunc(self, func, buf, offset, dsg, indent, indentStep):
+   def genPackUnpackFunc(self, func, buf, offset, operation, dsg, indent, indentStep):
       indent+=indentStep
       packLen=0
       code=C.block()
@@ -108,7 +132,7 @@ class NodeGenerator:
       val=func.arguments[0]
             
       codeBlock=C.sequence()
-      packLen=self.genPackItem(codeBlock, buf, val, dsg, localvar, offset, indent, indentStep)
+      packLen=self.genPackUnpackItem(codeBlock, buf, operation, val, dsg, localvar, offset, indent, indentStep)
       initializer=C.initializer(None,['(uint16)%du'%offset,'(uint16)%du'%packLen])
       if 'p' in localvar:
          code.append(C.statement(localvar['p'],indent=indent))
@@ -116,12 +140,18 @@ class NodeGenerator:
          if k=='p' or k=='buf':
             continue
          else:
-            code.append(C.statement(v,indent=indent))      
-      code.append(C.statement('apx_nodeData_lockOutPortData(&m_nodeData)', indent=indent))
+            code.append(C.statement(v,indent=indent))
+      if operation=='pack':
+         code.append(C.statement('apx_nodeData_lockOutPortData(&m_nodeData)', indent=indent))
+      else:
+         code.append(C.statement('apx_nodeData_lockInPortData(&m_nodeData)', indent=indent))
       if 'bufptr' in localvar:
          code.append(C.statement('%s=&%s[%d]'%(localvar['bufptr'].name,buf.name,offset),indent=indent))      
       code.extend(codeBlock)
-      code.append(C.statement(C.fcall('outPortData_writeCmd', params=[offset, packLen]),indent=indent))
+      if operation=='pack':      
+         code.append(C.statement(C.fcall('outPortData_writeCmd', params=[offset, packLen]),indent=indent))
+      else:
+         code.append(C.statement('apx_nodeData_unlockInPortData(&m_nodeData)', indent=indent))
       code.append(C.statement('return E_OK',indent=indent))
       indent-=indentStep
       return code,packLen
@@ -141,10 +171,6 @@ class NodeGenerator:
       headerFile.code.append(C.statement(initFunc))
       headerFile.code.append(C.statement(nodeDataFunc))
       headerFile.code.append(C.blank(1))
-#      self.inPortDataDefname = self.name.upper()+'_IN_PORT_DATA_LEN'
-#      self.outPortDataDefname = self.name.upper()+'_OUT_PORT_DATA_LEN'
-#      headerFile.code.append(C.define(self.inPortDataDefname,str(0)+'u'))
-#      headerFile.code.append(C.define(self.outPortDataDefname,str(packLen)+'u'))
       
       #write function prototypes for write functions
       headerFile.code.append(C.blank(1))
@@ -181,26 +207,43 @@ class NodeGenerator:
       code.append(C.line('//////////////////////////////////////////////////////////////////////////////'))
       code.append(C.line('// LOCAL FUNCTIONS'))
       code.append(C.line('//////////////////////////////////////////////////////////////////////////////'))
-      code.append(C.statement('static void outPortData_writeCmd(apx_offset_t offset, apx_size_t len )'))
+      if outPortDataLen>0:
+         code.append(C.statement('static void outPortData_writeCmd(apx_offset_t offset, apx_size_t len )'))
 
       code.append(C.line('//////////////////////////////////////////////////////////////////////////////'))
       code.append(C.line('// LOCAL VARIABLES'))
       code.append(C.line('//////////////////////////////////////////////////////////////////////////////'))
       
-      databuf=C.variable('m_outPortdata','uint8', static=True, array='APX_OUT_PORT_DATA_LEN')
-      initData=C.variable('m_outPortInitData','uint8_t', static=True, array='APX_OUT_PORT_DATA_LEN')
-      code.append(str(initData)+'= {')
-      #TODO: implement support for init values
-      #initBytes=[]
-      #for port in requirePorts:
-      #   initBytes.extend(list(port['initValue']))
-      #assert(len(initBytes) == databuf.array)
-      code.append('   0')
-      code.append('};')      
-      code.append(C.blank(1))
-    
-      code.append(C.statement(databuf))
-      code.append(C.statement(C.variable('m_outPortDirtyFlags','uint8_t', static=True, array='APX_OUT_PORT_DATA_LEN')))
+      if (outPortDataLen) > 0:
+         outDatabuf=C.variable('m_outPortdata','uint8', static=True, array='APX_OUT_PORT_DATA_LEN')
+         outInitData=C.variable('m_outPortInitData','uint8_t', static=True, const=True, array='APX_OUT_PORT_DATA_LEN')
+         code.append(str(outInitData)+'= {')
+         #TODO: implement support for init values
+         #initBytes=[]
+         #for port in requirePorts:
+         #   initBytes.extend(list(port['initValue']))
+         #assert(len(initBytes) == databuf.array)
+         code.append('   0')
+         code.append('};')      
+         code.append(C.blank(1))       
+         code.append(C.statement(outDatabuf))
+         code.append(C.statement(C.variable('m_outPortDirtyFlags','uint8_t', static=True, array='APX_OUT_PORT_DATA_LEN')))
+      else:
+         outDatabuf,outInitData = None,None         
+      
+      if (inPortDataLen) > 0:
+         inDatabuf=C.variable('m_inPortdata','uint8', static=True, array='APX_IN_PORT_DATA_LEN')
+         inInitData=C.variable('m_inPortInitData','uint8_t', static=True, const=True, array='APX_IN_PORT_DATA_LEN')
+         code.append(str(inInitData)+'= {')
+         #TODO: implement support for init values
+         code.append('   0')
+         code.append('};')      
+         code.append(C.blank(1))       
+         code.append(C.statement(inDatabuf))
+         code.append(C.statement(C.variable('m_inPortDirtyFlags','uint8_t', static=True, array='APX_OUT_PORT_DATA_LEN')))
+      else:
+         inDatabuf,inInitData = None,None
+      
       code.append(C.statement(C.variable('m_nodeData','apx_nodeData_t',static=True)))
       code.append(C.line('static const char *m_apxDefinitionData='))
       for line in node.text.split('\n'):
@@ -216,10 +259,23 @@ class NodeGenerator:
       code.append(C.line('//////////////////////////////////////////////////////////////////////////////'))
 
       sourceFile.code.append(initFunc)
-      body=C.block(innerIndent=indentStep)      
-      body.append(C.statement('memcpy(&%s[0],&%s[0],sizeof(%s))'%(databuf.name,initData.name,initData.name)))
-      body.append(C.statement('memset(&m_outPortDirtyFlags[0],0,sizeof(m_outPortDirtyFlags))'))
-      body.append(C.statement('apx_nodeData_create(&m_nodeData, "%s", (const uint8_t*) &m_apxDefinitionData[0], APX_DEFINITON_LEN, 0, 0, APX_IN_PORT_DATA_LEN, &m_outPortdata[0], &m_outPortDirtyFlags[0], APX_OUT_PORT_DATA_LEN)'%(node.name)))
+      body=C.block(innerIndent=indentStep)
+      if inPortDataLen>0:
+         body.append(C.statement('memcpy(&m_inPortdata[0], &m_inPortInitData[0], APX_IN_PORT_DATA_LEN)'))
+         body.append(C.statement('memset(&m_inPortDirtyFlags[0], 0, sizeof(m_inPortDirtyFlags))'))
+      if outPortDataLen>0:
+         body.append(C.statement('memcpy(&m_outPortdata[0], &m_outPortInitData[0], APX_OUT_PORT_DATA_LEN)'))
+         body.append(C.statement('memset(&m_outPortDirtyFlags[0], 0, sizeof(m_outPortDirtyFlags))'))
+      args = ['&m_nodeData', '"%s"'%(node.name), '(uint8_t*) &m_apxDefinitionData[0]', 'APX_DEFINITON_LEN']
+      if inPortDataLen>0:
+         args.extend(['&m_inPortdata[0]', '&m_inPortDirtyFlags[0]', 'APX_IN_PORT_DATA_LEN'])
+      else:
+         args.extend(['0','0','0'])
+      if outPortDataLen>0:         
+         args.extend(['&m_outPortdata[0]', '&m_outPortDirtyFlags[0]', 'APX_OUT_PORT_DATA_LEN'])
+      else:
+         args.extend(['0','0','0'])
+      body.append(C.statement('apx_nodeData_create(%s)'%(', '.join(args))))
       body.append(C.line('#ifdef APX_POLLED_DATA_MODE', indent=0))
       body.append(C.statement('rbfs_create(&m_outPortDataCmdQueue, &m_outPortDataCmdBuf[0], APX_NUM_OUT_PORTS, APX_DATA_WRITE_CMD_SIZE)'))
       body.append(C.line('#endif', indent=0))
@@ -236,14 +292,19 @@ class NodeGenerator:
     
       for signal in signalInfo:
          sourceFile.code.append(signal.func)
-         body,packLen=self.genPackFunc(signal.func,databuf,signal.offset,signal.dsg.resolveType(),indent,indentStep)      
+         if signal.operation == 'pack':
+            databuf = outDatabuf
+         else:
+            databuf = inDatabuf
+         body,packLen=self.genPackUnpackFunc(signal.func, databuf, signal.offset, signal.operation, signal.dsg.resolveType(), indent, indentStep)
          code.append(body)
          code.append(C.blank())
       
       code.append(C.line('//////////////////////////////////////////////////////////////////////////////'))
       code.append(C.line('// LOCAL FUNCTIONS'))
       code.append(C.line('//////////////////////////////////////////////////////////////////////////////'))
-      code.append('''static void outPortData_writeCmd(apx_offset_t offset, apx_size_t len )
+      if outPortDataLen>0:
+         code.append('''static void outPortData_writeCmd(apx_offset_t offset, apx_size_t len )
 {
    if ( (apx_nodeData_getDataWriteMode(&m_nodeData) == true) && (m_outPortDirtyFlags[offset] == 0) )
    {      
@@ -275,12 +336,11 @@ class NodeGenerator:
       for port in node.requirePortList:
          is_pointer=False        
          func = C.function("%s_Read_%s"%(name,port.name),"Std_ReturnType")
-         if port.dsg.isComplexType(node.typeList):
-            is_pointer=True
+         is_pointer=True
          func.add_arg(C.variable('val',port.dsg.typename(node.typeList),pointer=is_pointer))         
          packLen=port.dsg.packLen(node.typeList)
          port.dsg.typeList= node.typeList
-         signalInfo.append(SignalInfo(offset,packLen,func,port.dsg,0)) #TODO: implement init value
+         signalInfo.append(SignalInfo(offset,packLen,func,port.dsg,'unpack', 0)) #TODO: implement init value
          inPortDataLen+=packLen
          offset+=packLen
       #provide ports (out ports)
@@ -288,12 +348,12 @@ class NodeGenerator:
       for port in node.providePortList:
          is_pointer=False        
          func = C.function("%s_Write_%s"%(name,port.name),"Std_ReturnType")
-         if port.dsg.isComplexType(node.typeList):
+         if port.dsg.isComplexType(node.typeList) and not port.dsg.isArray(node.typeList):
             is_pointer=True    
          func.add_arg(C.variable('val',port.dsg.typename(node.typeList),pointer=is_pointer))         
          packLen=port.dsg.packLen(node.typeList)
          port.dsg.typeList= node.typeList
-         signalInfo.append(SignalInfo(offset,packLen,func,port.dsg,0)) #TODO: implement init value
+         signalInfo.append(SignalInfo(offset,packLen,func,port.dsg,'pack',0)) #TODO: implement init value
          outPortDataLen+=packLen
          offset+=packLen
             
